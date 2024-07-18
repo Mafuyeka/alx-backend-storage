1e1ac2f0d335443650c2e3e9467e05722869abfb
Tshepiso Rosa Mafuyeka <tshepisoroshina@gmail.com>
2024-07-18 07:58:54 -0700

    Updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-"""
-Caching request module
+""" A caching request module
 """
 import redis
 import requests
 from functools import wraps
-from typing import Callable
+from typing  import Callable, Any
 
 
 def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
+    """ getter for get_page
     """
     @wraps(fn)
     def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
+        """
+        wrapper that checks if a url's data is cached
+        and tracks how many times the get_page is called.
         """
         client = redis.Redis()
         client.incr(f'count:{url}')
@@ -30,7 +29,12 @@ def track_get_page(fn: Callable) -> Callable:
 
 @track_get_page
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
+    """
+    Makes a http request to a given endpoint
+    params:
+        url - the url of the web page to retreive
+    Returns:
+        The content of the web page.
     """
     response = requests.get(url)
     return response.text
